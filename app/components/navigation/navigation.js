517c8fce27ff2f6b9292b2d0a31f6a19259da48c
William Butt <william.butt@starcomww.com>
2014-06-10 20:36:12 -0400

    strip out SK stuff

--- app/components/navigation/navigation.js
+++ app/components/navigation/navigation.js
@@ -3,31 +3,8 @@
 var APP = window.APP = window.APP || {};
 
 APP.navigation = (function(){
-    var $menuOpenLink = $('.menu_link--js_open'),
-        $menuCloseLink = $('.menu_link--js_close'),
-        $nav = $('.navigation ul');
-
-    var menuOpen = function(){
-        $nav.slideDown();
-        $menuOpenLink.hide();
-        $menuCloseLink.show();
-    };
-
-    var menuClose = function(){
-        $nav.slideUp();
-        $menuOpenLink.show();
-        $menuCloseLink.hide();
-    };
-
     var bindEventsToUI = function() {
-        $menuOpenLink.on('click',function(event){
-            event.preventDefault();
-            menuOpen();
-        });
-        $menuCloseLink.on('click',function(event){
-            event.preventDefault();
-            menuClose();
-        });
+
     };
 
     var init = function() {
